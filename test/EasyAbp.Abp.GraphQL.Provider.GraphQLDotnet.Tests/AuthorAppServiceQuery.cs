ff273316946b1cbf6f7fc7a39fd560517bc001fb
gdlcf88 <gdlcf88@outlook.com>
2024-08-02 16:30:08 +0800

    Create scope in `AppServiceQuery`

--- test/EasyAbp.Abp.GraphQL.Provider.GraphQLDotnet.Tests/AuthorAppServiceQuery.cs
+++ test/EasyAbp.Abp.GraphQL.Provider.GraphQLDotnet.Tests/AuthorAppServiceQuery.cs
@@ -7,41 +7,45 @@ using GraphQL;
 using GraphQL.Types;
 using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Application.Dtos;
-using Volo.Abp.Application.Services;
 using Volo.Abp.DependencyInjection;
 
 namespace EasyAbp.Abp.GraphQL;
 
 [ExposeServices(typeof(AppServiceQuery<IAuthorAppService, AuthorDto, AuthorDto, int, PagedAndSortedResultRequestDto>))]
 public class AuthorAppServiceQuery :
-    AppServiceQuery<IAuthorAppService, AuthorDto, AuthorDto, int, PagedAndSortedResultRequestDto>
+    AppServiceQuery<IAuthorAppService, AuthorDto, AuthorDto, int, PagedAndSortedResultRequestDto>, IObjectGraphType
 {
     public AuthorAppServiceQuery(IServiceProvider serviceProvider)
     {
         const string entityName = "Author";
-            
+
         Name = entityName + "Query";
 
-        var appService = serviceProvider.GetRequiredService<IAuthorAppService>();
-            
-        var readOnlyAppService =
-            (IReadOnlyAppService<AuthorDto, AuthorDto, int, PagedAndSortedResultRequestDto>)appService;
+        Field<GraphQLGenericType<AuthorDto>>(entityName)
+            .Arguments(new QueryArguments(
+                new QueryArgument(typeof(NonNullGraphType<IntGraphType>)) { Name = "id" }))
+            .ResolveAsync(async context =>
+            {
+                using var scope = serviceProvider.CreateScope();
 
-        FieldAsync<GraphQLGenericType<AuthorDto>>(entityName,
-            arguments: new QueryArguments(
-                new QueryArgument(typeof(NonNullGraphType<IntGraphType>)) { Name = "id" }),
-            resolve: async context =>
-                await readOnlyAppService.GetAsync(context.GetArgument<int>("id"))
-        );
+                var service = scope.ServiceProvider.GetRequiredService<IAuthorAppService>();
 
-        FieldAsync<PagedResultDtoType<AuthorDto>>($"{entityName}List",
-            arguments: new QueryArguments(
+                return await service.GetAsync(context.GetArgument<int>("id"));
+            });
+
+        Field<PagedResultDtoType<AuthorDto>>($"{entityName}List")
+            .Arguments(new QueryArguments(
                 new QueryArgument<GraphQLInputGenericType<PagedAndSortedResultRequestDto>>
-                    { Name = "input", DefaultValue = Activator.CreateInstance<PagedAndSortedResultRequestDto>() }),
-            resolve: async context =>
-                await readOnlyAppService.GetListAsync(context.GetArgument<PagedAndSortedResultRequestDto>("input"))
-        );
-            
+                    { Name = "input", DefaultValue = Activator.CreateInstance<PagedAndSortedResultRequestDto>() }))
+            .ResolveAsync(async context =>
+            {
+                using var scope = serviceProvider.CreateScope();
+
+                var service = scope.ServiceProvider.GetRequiredService<IAuthorAppService>();
+
+                return await service.GetListAsync(context.GetArgument<PagedAndSortedResultRequestDto>("input"));
+            });
+
         // An extra field.
         Field<StringGraphType>("ping", resolve: _ => "pong");
     }
